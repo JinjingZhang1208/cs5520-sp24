fb2d040c20867f7c675dc6061dd829c61d79b481
Jinjing Zhang <zhang.jinjin@northeastern.edu>
2024-01-16 13:44:35 -0800

    Changed welcome message

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable semi */
+/* eslint-disable react/react-in-jsx-scope */
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+export default function App () {
+  const appName = 'My Awersome App';
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
