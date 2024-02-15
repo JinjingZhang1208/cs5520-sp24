07f45d7b53aab0f8f26d3bebc895dcd51548ee4c
Jinjing Zhang <zhang.jinjin@northeastern.edu>
2024-02-15 13:54:31 -0800

    update

--- components/Input.js
+++ components/Input.js
@@ -13,7 +13,7 @@ export default function Input({ inputHandler, modalVisible, dismissModal }) {
   const [text, setText] = useState("");
   // callback handler
   function changeTextHandler(changedText) {
-    console.log("user is typing ", changedText);
+    // console.log("user is typing ", changedText);
 
     setText(changedText);
   }
@@ -23,31 +23,39 @@ export default function Input({ inputHandler, modalVisible, dismissModal }) {
     setText("");
   }
   function cancelHandler() {
+    setText("");
+
     // hide the modal
     dismissModal();
   }
   return (
-    <Modal visible={modalVisible}>
+    <Modal visible={modalVisible} animationType="slide" transparent={true}>
       <View style={styles.container}>
-        <Image
-          source={{
-            uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
-          }}
-          style={styles.image}
-        />
-        <Image source={require("../assets/goal.png")} style={styles.image} />
-        <TextInput
-          placeholder="Type something"
-          style={styles.input}
-          value={text}
-          onChangeText={changeTextHandler}
-        />
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonView}>
-            <Button title="Cancel" onPress={cancelHandler} />
-          </View>
-          <View style={styles.buttonView}>
-            <Button title="Confirm" onPress={confirmHandler} disabled={!text} />
+        <View style={styles.modalView}>
+          <Image
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+            }}
+            style={styles.image}
+          />
+          <Image source={require("../assets/goal.png")} style={styles.image} />
+          <TextInput
+            placeholder="Type something"
+            style={styles.input}
+            value={text}
+            onChangeText={changeTextHandler}
+          />
+          <View style={styles.buttonsContainer}>
+            <View style={styles.buttonView}>
+              <Button title="Cancel" onPress={cancelHandler} />
+            </View>
+            <View style={styles.buttonView}>
+              <Button
+                title="Confirm"
+                onPress={confirmHandler}
+                disabled={!text}
+              />
+            </View>
           </View>
         </View>
       </View>
@@ -60,6 +68,13 @@ const styles = StyleSheet.create({
     width: "30%",
     margin: 5,
   },
+
+  modalView: {
+    backgroundColor: "#999",
+    borderRadius: 20,
+    padding: "10%",
+    alignItems: "center",
+  },
   buttonsContainer: { flexDirection: "row" },
   input: {
     borderBottomWidth: 2,
@@ -68,7 +83,6 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: "#aaa",
     alignItems: "center",
     justifyContent: "center",
   },
